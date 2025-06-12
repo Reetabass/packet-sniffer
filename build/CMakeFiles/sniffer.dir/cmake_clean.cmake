525134c57cad204b46e6be71824b3a991ad025e9
reetabass <michael.basangan@student.uts.edu.au>
2025-06-12 11:26:18 +1000

    Basic sniffing can see packet length

---
+++ build/CMakeFiles/sniffer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sniffer.dir/src/main.c.o"
+  "CMakeFiles/sniffer.dir/src/main.c.o.d"
+  "sniffer"
+  "sniffer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sniffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
